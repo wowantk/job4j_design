6af1efcbd99b3a29c7cfc0374aec9085f8325022
wowantk <vova.drobot@icloud.com>
2020-09-27 12:07:53 +0300

    EvenIter

---
+++ chapter_001/src/main/java/EvenIt.java
@@ -0,0 +1,58 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+public class EvenIt implements  Iterator<Integer>{
+        /**
+         * field of array.
+         */
+        private final int[] data;
+        /**
+         * field of counter.
+         */
+        private int point = 0;
+
+        /**
+         *constructor of object iterator that take array.
+         * @param data object array
+         */
+        public EvenIt(int[] data) {
+            this.data = data;
+        }
+
+        /**
+         * Checker for all next element of array.
+         * @return true if array contains even number
+         * after checker or false when array doesnt contains even number
+         */
+        @Override
+        public boolean hasNext() {
+            int co = 0;
+            for (int i = point; i < data.length; i++) {
+                if (data[i] % 2 == 0) {
+                    co++;
+                }
+            }
+            return co > 0;
+        }
+
+        /**
+         *
+         * @return next element or increase counter
+         */
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            if (data[point] % 2 != 0) {
+                point++;
+            }
+
+            return data[point++];
+        }
+
+        @Override
+        public void remove() {
+
+        }
+}
