ea67201288cacd2569a77849124fae630ef3d855
wowantk <vova.drobot@icloud.com>
2020-10-13 00:02:51 +0300

    Generic

---
+++ chapter_001/src/main/java/Genericc/SimpleArray.java
@@ -0,0 +1,35 @@
+package Genericc;
+
+import java.util.Objects;
+
+public class SimpleArray <T>{
+    private Object[] obj_array;
+    public final int length;
+    public SimpleArray(int length) {
+        this.obj_array = new Object [length];
+        this.length = length;
+    }
+
+    public void setMode(int i ,T mode ){
+        obj_array[i] = mode;
+  }
+  public void add(T mode){
+      for (int i = 0; i <length; i++) {
+          if(obj_array[i]  == null) obj_array[i] = mode;
+          else continue;
+
+      }
+  }
+
+  public void delete(int i){
+        Object[] result = new Object[length-1];
+        System.arraycopy(obj_array,0,result,0,i);
+        System.arraycopy(obj_array, i + 1, result, i, length - i - 1);
+        obj_array = result;
+  }
+  public T get(int i){
+      @SuppressWarnings("unchecked")
+      final T t = (T)obj_array[i];
+      return t;
+  }
+}
